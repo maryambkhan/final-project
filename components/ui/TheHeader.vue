037ff87ca8ef240e93e46f2cdbf1c2f72f3aaa13
Maryam Khan <maryam.b.khan@edu.sait.ca>
2022-04-12 15:12:20 -0600

    fix nav

--- components/ui/TheHeader.vue
+++ components/ui/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <header class="shadow-md shadow-slate-600">
-    <h1 class="flex justify-center font-serif text-4xl  ">ABK Photos</h1>
+  <header class="shadow-md shadow-slate-600 mb-10">
+    <h1 class="flex justify-center font-serif text-4xl ">ABK Photos</h1>
     <TheNavBar />
   </header>
 </template>
