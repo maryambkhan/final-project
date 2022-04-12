881261b483e3dd0659241a257d8c556588575c2b
Mahalakshmi <mahait07@gmail.com>
2022-04-12 14:58:08 -0600

    footer added

---
+++ components/ui/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="mt-10">
+<section class="mt-2 border border-slate-300 mx-10 "> </section>
+  <div class="grid grid-cols-2 gap-5 mt-3">
+    <section class="ml-10">
+    <h1 class="font-serif text-xl">&copy; ABK Photos 2022</h1>
+    </section>
+    <section class="justify-self-end mr-10">
+      <font-awesome-icon :icon="['fab', 'instagram']" class="mr-2 text-xl"/>
+      <font-awesome-icon :icon="['fab', 'facebook']"  class="mr-2 text-xl"/>
+      <font-awesome-icon :icon="['fas' , 'envelope']" class="mr-2 text-xl"/>
+
+    </section>
+  </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
 <TheHeader />
+<TheFooter />
 </div>
 </template>
 
