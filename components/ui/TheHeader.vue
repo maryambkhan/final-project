f117775bfd0ff7c2194e303951909261c4250e4d
Maryam Khan <maryam.b.khan@edu.sait.ca>
2022-04-12 15:19:27 -0600

    ficed shadow

--- components/ui/TheHeader.vue
+++ components/ui/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="shadow-md shadow-slate-600 mb-10">
+  <header class="shadow-sm shadow-slate-600 pb-2">
     <h1 class="flex justify-center font-serif text-4xl ">ABK Photos</h1>
     <TheNavBar />
   </header>
--- components/ui/TheNavBar.vue
+++ components/ui/TheNavBar.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="flex flex-wrap justify-center mt-5">
-    <NuxtLink to="/" class="px-10  text-lg font-semibold hover:bg-gray-400">HOME</NuxtLink>
-    <NuxtLink to="/gallery" class="px-10  text-lg font-semibold hover:text-gray-400">GALLERY</NuxtLink>
-    <NuxtLink  to="/about" class="px-10  text-lg font-semibold hover:bg-gray-400" >ABOUT</NuxtLink>
-    <NuxtLink to="/contact" class="px-10  text-lg font-semibold hover:bg-gray-400">CONTACT</NuxtLink>
+    <NuxtLink to="/" class="px-4  text-lg font-semibold hover:bg-gray-400">HOME</NuxtLink>
+    <NuxtLink to="/gallery" class="px-4 text-lg font-semibold hover:text-gray-400">GALLERY</NuxtLink>
+    <NuxtLink  to="/about" class="px-4  text-lg font-semibold hover:bg-gray-400" >ABOUT</NuxtLink>
+    <NuxtLink to="/contact" class="px-4  text-lg font-semibold hover:bg-gray-400">CONTACT</NuxtLink>
   </nav>
 </template>
 
