aa5c292ba7a6220449edabbaa5fb951e2906ae6d
Maryam Khan <maryam.b.khan@edu.sait.ca>
2022-04-13 09:09:58 -0600

    done nav-bar

--- components/ui/TheNavBar.vue
+++ components/ui/TheNavBar.vue
@@ -2,15 +2,15 @@
   <div class="sm:flex sm:justify-center">
     <div class="sm:hidden mt-2">
       <button @click="isOpen = !isOpen" type="button" class="hover:text-slate-600">
-        <span class="font-semibold">Menu</span>
+        <span class="font-semibold text-xl ml-4">Menu</span>
       </button>
     </div>
     
     <div :class="isOpen ? 'block' : 'hidden'" class="items-center px-2 pt-2 pb-4 sm:flex">
-    <NuxtLink to="/" class="block px-2 py-1 text-lg font-semibold rounded hover:bg-gray-300">HOME</NuxtLink>
-    <NuxtLink to="/gallery" class="block  px-2 py-1 text-lg font-semibold rounded hover:bg-gray-300">GALLERY</NuxtLink>
-    <NuxtLink  to="/about" class="block   px-2 py-1 text-lg font-semibold rounded hover:bg-gray-300" >ABOUT</NuxtLink>
-    <NuxtLink to="/contact" class="block px-2 py-1  text-lg font-semibold rounded hover:bg-gray-300">CONTACT</NuxtLink>
+    <NuxtLink to="/" class="block px-2 py-1 lg:text-xl font-semibold rounded hover:bg-gray-300">HOME</NuxtLink>
+    <NuxtLink to="/gallery" class="block  px-2 py-1 lg:text-xl font-semibold rounded hover:bg-gray-300">GALLERY</NuxtLink>
+    <NuxtLink  to="/about" class="block   px-2 py-1 lg:text-xl font-semibold rounded hover:bg-gray-300" >ABOUT</NuxtLink>
+    <NuxtLink to="/contact" class="block px-2 py-1  lg:text-xl font-semibold rounded hover:bg-gray-300">CONTACT</NuxtLink>
   </div>
   </div>
 </template>
