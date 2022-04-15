6c55eceda4db70abd4a797882530f8ddeaf76f75
Maryam Khan <maryam.b.khan@edu.sait.ca>
2022-04-15 15:40:03 -0600

    made contact form responsive

--- pages/contact.vue
+++ pages/contact.vue
@@ -12,45 +12,59 @@
     </div>
     <!--form-->
      <section class="mx-auto mt-2 bg-gray-300 max-w-6xl py-20 px-12 lg:px-24">
-  <form action="https://formspree.io/f/mrgjyaed" method="POST"  class="flex justify-center  mt-10">
+  <form action="https://formspree.io/f/mrgjyaed" method="POST"  class="flex justify-center">
     <fieldset>
         <!--first name-->
-        <div class="flex gap-2">
+           
+     <div class="-mx-3 md:flex">
       <div>
         <label for="user" class="after:content-['*'] after:ml-0.5 after:text-red-700 block text-lg  text-slate-700 font-bold">First Name</label>
-        <input type="text" name="firstname" id="user"  class="mt-2 px-3 py-2 bg-white rounded-md border  border-slate-500  ml-2" >
-      </div>
+        <input type="text" name="firstname" id="user"  required="" class=" mt-2 px-3 py-2 bg-white rounded-md border  border-slate-500  ml-2" >
+        </div>
+        
       <!--last name-->
-      <div>
+        <div class="md:w-1/2">
         <label for="user" class=" after:content-['*'] after:ml-0.5 after:text-red-700 block text-lg  text-slate-700 font-bold">Last Name</label>
-        <input type="text" name="lastname" id="lastname"  class="mt-2 px-3 py-2 bg-white rounded-md border border-slate-500  ml-2 " >
+        <input type="text" name="lastname" id="lastname"  required="" class="mt-2 px-3 py-2 bg-white rounded-md border border-slate-500  ml-2 " >
       </div>
         </div>
+
       <!--email-->
-      <div class="flex gap-2">
+       <div class="-mx-3 md:flex">
       <div>
         <label for="email" class="mt-2 after:content-['*'] after:ml-0.5 after:text-red-700 block text-lg  text-slate-700 font-bold">Email</label>
-        <input type="email" name="email" id="email" placeholder="you@example.com" class="mt-2 px-3 py-2 bg-white rounded-md border  border-slate-500  ml-2 " >
+        <input type="email" name="email" id="email" placeholder="you@example.com" required="" class="mt-2 px-3 py-2 bg-white rounded-md border  border-slate-500  ml-2 " >
       </div>
+
       <!--phone number-->
-      <div>
+      <div class="md:w-1/2">
         <label for="phone" class="mt-2 after:content-['*'] after:ml-0.5 after:text-red-700 block text-lg  text-slate-700 font-bold">Phone</label>
-        <input type="text" name="phone"  id="phone"  class="mt-2 px-3 py-2 bg-white rounded-md border  border-slate-500  ml-2 " >
+        <input type="text" name="phone"  id="phone"  placeholder="###-###-####" required="" class="mt-2 px-3 py-2 bg-white rounded-md border  border-slate-500  ml-2 " >
       </div>
       </div>
+
       <!--subject-->
-      <div>
+        <div class="-mx-3 md:flex">
+         <div class="md:w-full">
         <label for="subject" class="mt-2 after:content-['*'] after:ml-0.5 after:text-red-700 block text-lg  text-slate-700 font-bold">Subject</label>
-        <input type="subject" name="subject" id="subject"  class="w-full mt-2 px-3 py-2 bg-white rounded-md border  border-slate-500  ml-2 " >
+        <input type="subject" name="subject" id="subject"  required="" class="sm:w-1/2 md:w-full mt-2 px-3 py-2 bg-white rounded-md border  border-slate-500  ml-2 " >
       </div>
+        </div>
+       
       <!--message-->
-      <div>
+      <div class="-mx-3 md:flex">
+      <div class="md:w-full">
       <label for="message" class="mt-2 after:content-['*'] after:ml-0.5 after:text-red-700 block text-lg  text-slate-700 font-bold">Message</label>
-      <textarea type="message" name="message" id="message" rows="4" cols="20"  class="w-full mt-2 px-3 py-2 bg-white border rounded-md border-slate-500  ml-2 " > 
+      <textarea type="message" name="message" id="message" rows="4" cols="20"  required="" class="sm:w-1/2 md:w-full mt-2 px-3 py-2 bg-white border rounded-md border-slate-500 ml-2 " > 
       </textarea>
       </div>
+      </div>
       <!--button-->
-      <button   type="submit" class="w-full mt-10 bg-black text-white p-2 text-2xl rounded-md">Submit</button>
+      <div class="-mx-3 md:flex mt-2">
+          <div class="md:w-full px-3">
+      <button  type="submit" id="submit" class="md:w-full mt-10 bg-black text-white p-2 text-2xl rounded-md"><a href="thanks">Submit</a></button>
+          </div>
+      </div>
     </fieldset>
   </form>
   </section>
@@ -60,4 +74,6 @@
 </template>
 
 <script setup>
+
+
 </script>
---
+++ pages/thanks.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>THANK YOU!</h1>
+		<button type="submit" id="submit" class="w-full mt-10 bg-black text-white p-2 text-2xl rounded-md"><a href="index">Go Home </a>
+</button>
+  </div>
+</template>
+
+<script setup>
+
+</script>
